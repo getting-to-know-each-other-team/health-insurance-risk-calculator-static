9a169d54ae46c624b998336639c86275912d500e
Matthew Shouse <mshouse8624@gmail.com>
2024-10-18 20:27:01 -0500

    Added a methods to calculate the BMI as well as the insurance points Shouse

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// declare constant variable to hold the website link 
+const url = "health-insurance-risk-calculator-nodejs-dpbgb6dsgpadajaj.centralus-01.azurewebsites.net"
+// const url = "http://localhost:3000"
+
 // Function to ping the server and check if it's awake
 function pingServer() {
   fetch('http://localhost:3000/ping')
@@ -13,4 +17,31 @@ function pingServer() {
 }
 
 // Automatically ping the server when the page loads
-window.onload = pingServer;
+window.onload = pingServer();
+
+/* This method will calculate the BMI when the user presses a button.  It will pass the 3 values
+ that were entered by the user to the API server and then the server will take those and return 
+ the value of the BMI calculations
+*/
+async function calculateBmi() {
+    // grabs the values for the bmi calculations
+    let heightFeet = document.getElementById("feet").value;
+    let heightInches = document.getElementById("inches").value;
+    let weight = document.getElementById("pounds").value;
+
+    // calls the server API to calculate the bmi
+    const fetchString = url + "/calculate-bmi?feet="+heightFeet+"&inches="+heightInches+"&weight="+weight;
+    const response = await fetch(fetchString)
+    const responseText = await response.text()
+
+    // this will then display the value of the bmi in the html 
+    document.getElementById("bmiResults").innerHTML = responseText
+}
+
+async function calculateInsurancePoints() {
+    // gets the values from the input html elements
+    let systolic = document.getElementById("systolic").value;
+    let diastolic = document.getElementById("diastolic").value;
+
+    
+}
